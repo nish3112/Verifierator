7b7541749ad6ed6086ab825a80f9c55dc73ade2f
Hetvi Shah <hetviiis2403@gmail.com>
2023-04-02 08:19:47 +0530

    frontend-2

--- kjsce_hack/kjsce-hack/src/components/Navbar.jsx
+++ kjsce_hack/kjsce-hack/src/components/Navbar.jsx
@@ -2,34 +2,46 @@ import { useState } from "react";
 import {logo} from "../assets";
 import {Link} from 'react-router-dom';
 
-const Navbar = () => {
+function Navbar() {
   const [active, setActive] = useState("Home");
-  const [toggle, setToggle] = useState(false);
-
+  const connectWallet = async() => {
+    if(typeof window!='undefined' && typeof window.ethereum!='undefined'){
+      try{
+      const accounts = await window.ethereum.request({method: "eth_requestAccounts"});
+      console.log(accounts[0]);
+    } catch(err){
+      console.error(err.message);
+    }
+  } else{
+    console.log("Please install MetaMask");
+  }
+  }
   return (
     <nav className="w-full flex py-6 justify-between items-center navbar">
-      
-      
+
+
       <img src={logo} alt="hoobank" className="w-[135px] h-[90px]" />
-  
-      <span style={{color:"white",fontSize:"22px"}}>erifierator</span>
+
+      <span style={{ color: "white", fontSize: "22px" }}>erifierator</span>
       <div className="nav-right">
         <div className="nav-list">
-        <ul className="list-none sm:flex hidden justify-end items-center flex-1"  style={{display:'flex',gap:'4rem',
-              color:'rgb(255, 255, 255)'}}>
-                    <li className="font-poppins font-normal cursor-pointer text-[16px]"><a href="/" >Home</a></li>
-                    <li className="font-poppins font-normal cursor-pointer text-[16px]"><a href="/register" >Register Device</a></li>
-                    <li className="font-poppins font-normal cursor-pointer text-[16px]"><a href="/check" >Check device</a></li>
-                    <li className="font-poppins font-normal cursor-pointer text-[16px]"><a href="/admin" >Admin</a></li>
+          <ul className="list-none sm:flex hidden justify-end items-center flex-1" style={{
+            display: 'flex', gap: '4rem',
+            color: 'rgb(255, 255, 255)'
+          }}>
+            <li className="font-poppins font-normal cursor-pointer text-[16px]"><a href="/">Home</a></li>
+            <li className="font-poppins font-normal cursor-pointer text-[16px]"><a href="/register">Register Device</a></li>
+            <li className="font-poppins font-normal cursor-pointer text-[16px]"><a href="/check">Check device</a></li>
+            <li className="font-poppins font-normal cursor-pointer text-[16px]"><a href="/admin">Admin</a></li>
 
-      </ul>
+          </ul>
+        </div>
+        <button className="nav-bttn" onClick={connectWallet}>Connect to Wallet</button>
       </div>
-      <button className="nav-button">Connect to wallet</button>
-      </div>
-     
+
 
     </nav>
   );
-};
+}
 
 export default Navbar;
